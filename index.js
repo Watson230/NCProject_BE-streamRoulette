cdcd9b596020839d1f2a999ab26c29b80e06a79e
Dave <dave@Nicks-MBP-2.home>
2018-05-03 21:41:29 +0100

    changed port config for heroku deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server').app;
-const PORT = require('./config').PORT[process.env.NODE_ENV];
+const PORT = require('./config').PORT[process.env];
 const apirouter =  require('./route/apiRoute').router
 const mongooseConnect = require('./server').mongooseConnect
 const bodyParser = require('body-parser');
@@ -15,6 +15,6 @@ app.use(bodyParser.json());
 app.use('/api',apirouter)
 
 
-app.listen(PORT, function () {
-    console.log(`listening on port ${PORT}`);
+app.listen(process.env, function () {
+    console.log(`listening on port ${process.env}`);
   })
