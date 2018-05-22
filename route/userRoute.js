23e24d64bee88617049917f5fcebb0717506efe8
Dave <davidwatson2323@gmail.com>
2018-05-22 23:06:10 +0100

    fixed error with userctrl import

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const{postUser,getUser}= require('../userCtrl');
+const{postUser,getUser}= require('../controllers/userCtrl');
 
 router.post('', postUser);
 
