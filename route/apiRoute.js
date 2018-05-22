42b22f0f05e6733f24545f0b1c2808c51aecb5cf
Dave <davidwatson2323@gmail.com>
2018-05-22 23:11:30 +0100

    fixed issues with api route

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const userRoute = require('./userRoute');
-const filmRoute =  require('./filmRoute');
-const searchRoute = require('./searchRoute');
+const userRoute = require('./userRoute').router;
+const filmRoute =  require('./filmRoute').router;
+const searchRoute = require('./searchRoute').router;
 
 // router.get('/', );
 
 
-router.use('/film',filmRoute);
+router.use('/film', filmRoute);
 router.use('/search', searchRoute);
 router.use('/user', userRoute);
 
